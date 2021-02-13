0be0b7b721e9d73299113a6cfaafd9effa804854
Jakub Kubasiak <kubakubasiak@o2.pl>
2021-02-13 18:39:11 +0100

    added product list page

--- Cube.Blazor.Shop/Client/Program.cs
+++ Cube.Blazor.Shop/Client/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Cube.Blazor.Shop.Client.Services.ProductService;
 
 namespace Cube.Blazor.Shop.Client
 {
@@ -18,6 +19,7 @@ namespace Cube.Blazor.Shop.Client
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<IProductService, ProductService>();
 
             await builder.Build().RunAsync();
         }
