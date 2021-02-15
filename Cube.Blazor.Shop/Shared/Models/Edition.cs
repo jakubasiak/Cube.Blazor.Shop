f721dd7af8d2691c7b08577c9db14e10969ee544
Jakub Kubasiak <kubakubasiak@o2.pl>
2021-02-15 13:02:42 +0100

    added Edition to UI

--- Cube.Blazor.Shop/Shared/Models/Edition.cs
+++ Cube.Blazor.Shop/Shared/Models/Edition.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.Text.Json.Serialization;
 
 namespace Cube.Blazor.Shop.Shared.Models
 {
@@ -9,6 +9,7 @@ namespace Cube.Blazor.Shop.Shared.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        [JsonIgnore]
         public List<Product> Products { get; set; }
     }
 }
